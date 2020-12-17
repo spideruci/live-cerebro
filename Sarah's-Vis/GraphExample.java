ed895ea811c0cbdfa2c6bf6ea04f6e7bd5bb29b9
lij557 <lij557@gmail.com>
2020-12-17 15:56:19 -0800

    Updated graph to accept links. Refined layout.

--- Sarah's-Vis/GraphExample.java
+++ Sarah's-Vis/GraphExample.java
@@ -1,17 +1,11 @@
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
-import org.graphstream.ui.swing_viewer.DefaultView;
-import org.graphstream.ui.swing_viewer.ViewPanel;
 import org.graphstream.ui.view.Viewer;
-import org.graphstream.ui.view.ViewerListener;
-import org.graphstream.ui.view.ViewerPipe;
 import org.graphstream.ui.view.util.MouseManager;
 import org.graphstream.ui.graphicGraph.GraphicElement;
 import org.graphstream.ui.view.util.InteractiveElement;
 import org.graphstream.ui.view.*;
 import org.graphstream.ui.view.View;
-import org.spideruci.analysis.dynamic.api.LineProfiler;
-import java.util.concurrent.BlockingQueue;
 import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -19,93 +13,67 @@ import java.util.Scanner;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.event.MouseListener;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 public class GraphExample implements MouseListener{
 	static View view;
-	static JFrame frame;
-	static JLabel label;
-	public static void main(String args[]) {
-		BlockingQueue queue = LineProfiler.getQueue();
-		Thread consumer = new Thread(new Runnable(){
-			public void run() {
-				try {
-					String info = queue.take();
-					System.out.println("CONSUMED " + info);
 
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		try{
-			consumer.start();
-			consumer.join();
-		} catch(InterruptedException ex){
-			ex.printStackTrace();
-		}
-		System.out.println(queue.size());
+	static Graph graph;
+	public static void main(String args[]) {
 		new GraphExample();
 	}
 
 	public GraphExample(){
 		File lines = new File("output.txt");
-		Graph graph = new SingleGraph("Tutorial 1");
-		HashMap<String, String> map = new HashMap<>();
-
+		graph = new SingleGraph("Live Cerebro");
+		HashMap<String, Integer> edgeCountMap = new HashMap<>();
 		System.setProperty("org.graphstream.ui", "swing");
 
 		String prevNode = "";
 		try{
 			Scanner in = new Scanner(lines);
-			frame = new JFrame();
-			frame.setPreferredSize(new Dimension(600,600));
 			Viewer viewer = graph.display();
 			view = viewer.getDefaultView();
 			view.enableMouseOptions();
 			view.addListener("Mouse", (MouseListener)this);
-			label = new JLabel("Mouse");
-			frame.add(label);
-			frame.setVisible(true);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//			ViewerPipe fromViewer = viewer.newViewerPipe();
-//			ViewerEventListener viewerListener =
-//					new ViewerEventListener(fromViewer);
-//			viewer.getDefaultView().setMouseManager(viewerListener);
-//			viewer.getDefaultView().addMouseWheelListener(viewerListener);
-//			fromViewer.addViewerListener(viewerListener);
-//			fromViewer.addSink(this.graph);
-
-			//viewer.getDefaultView().setMouseManager(manager);
-			//viewer.enableAutoLayout();
-//			viewer.getDefaultView().setMouseManager(new MouseManager(EnumSet.of(InteractiveElement.EDGE, InteractiveElement.NODE, InteractiveElement.SPRITE)));
-			graph.setAttribute("stylesheet", "node { fill-mode: dyn-plain; fill-color: red, blue; }");
+			graph.setAttribute("stylesheet", "graph { fill-color: black; }" +
+					"node { fill-mode: gradient-radial; fill-color: rgba(204,255,255,228), rgba(204,255,255,8); }");
 			// figure out how to fade the colors from darker to lighter red
-			while(in.hasNextLine()){
+
+			while(in.hasNextLine()) {
 				String line = in.nextLine();
 				String[] lineArgs = line.split(" ");
-				String node = lineArgs[3];
-				map.put(node, lineArgs[1]);
-				Thread.sleep(1000);
-				if(graph.getNode(node) == null){
-					graph.addNode(node);
-					Node n = graph.getNode(node);
+				String node1 = lineArgs[1];
+				String node2 = lineArgs[3];
+				Thread.sleep(200);
+				if (graph.getNode(node1) == null) {
+					graph.addNode(node1);
+					Node n = graph.getNode(node1);
 					n.setAttribute("ui.label", "    " + n.getId());
-					n.setAttribute("ui.color", 0f);
-				} else{
-					Node n = graph.getNode(node);
-					n.setAttribute("ui.color", 0f);
-					Node prev = graph.getNode(prevNode);
-					prev.setAttribute("ui.color", 1f);
 				}
-				if(prevNode != "" && map.get(prevNode).equals(map.get(node)) && graph.getEdge(prevNode + node) == null){
-					graph.addEdge(prevNode + node, prevNode, node);
-					Node prev = graph.getNode(prevNode);
-					prev.setAttribute("ui.color", 1f);
+				if (graph.getNode(node2) == null) {
+					graph.addNode(node2);
+					Node n = graph.getNode(node2);
+					n.setAttribute("ui.label", "    " + n.getId());
 				}
-				prevNode = node;
 
+				if (graph.getEdge(node1 + " " + node2) == null) {
+					edgeCountMap.put(node1 + " " + node2, 1);
+					graph.addEdge(node1 + " " + node2, node1, node2);
+					Edge edge = graph.getEdge(node1 + " " + node2);
+					double weight = (3.3 - edgeCountMap.get(node1 + " " + node2));
+					edge.setAttribute("layout.weight", weight);
+					edge.setAttribute("ui.style", "size: 1px; fill-color: rgba(255,255,255,68);");
+				} else {
+					// compare this to the max and if getting it + 1 is greater, then update max
+					edgeCountMap.put(node1 + " " + node2, edgeCountMap.get(node1 + " " + node2) + 1);
+					int count = edgeCountMap.get(node1 + " " + node2);
+					Edge edge = graph.getEdge(node1 + " " + node2);
+					double weight = 3.3 - count;
+					edge.setAttribute("layout.weight", weight);
+					String size = String.valueOf(2 * count);
+					String str = "size: " + size + "px; fill-color: rgba(255,255,255,68);";
+					edge.setAttribute("ui.style", str);
+				}
 			}
 		}
 		catch(InterruptedException e){
@@ -114,7 +82,9 @@ public class GraphExample implements MouseListener{
 		catch(FileNotFoundException e){
 			System.out.println("Error: File Not Found Exception");
 		}
+
 	}
+
 
 	// getX() and getY() functions return the
 	// x and y coordinates of the current
@@ -152,7 +122,6 @@ public class GraphExample implements MouseListener{
 		EnumSet<InteractiveElement> set = EnumSet.of(InteractiveElement.NODE, InteractiveElement.SPRITE);
 		GraphicElement ge = view.findGraphicElementAt(set, e.getX(), e.getY());
 		System.out.println(ge.getId());
-		label.setText(ge.getId());
 		// getClickCount gives the number of quick,
 		// consecutive clicks made by the user
 		// show the point where the mouse is i.e
